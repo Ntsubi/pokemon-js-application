2792cad23eaf6de2b2f946d1a16d38e883aabb14
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-04 18:30:14 +0200

    Added 3 Pokemon objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [];
+let pokemonList = [
+    {name: 'Squirtle', height: 5, type: ['water', 'grass']},
+    {name: 'Diglet', height: 2, type: ['ground', 'trees']},
+    {name: 'Pidgey', height: 3, type: ['flying', 'poision']}
+];
