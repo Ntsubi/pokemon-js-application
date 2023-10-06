17c1e14403326e8645fb61a806297c390c6ad28b
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-06 16:12:26 +0200

    Created a conditional relating to height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 let pokemonList = [
-    {name: 'Squirtle', height: 5, type: ['water', 'grass']},
-    {name: 'Diglet', height: 2, type: ['ground', 'trees']},
-    {name: 'Pidgey', height: 3, type: ['flying', 'poision']}
-];
+    {name: 'Squirtle', 
+    height: 5, 
+    type: ['water', 'grass']
+    },
+    {name: 'Diglet', 
+    height: 2, 
+    type: ['ground', 'trees']
+    },
+    {name: 'Pidgey', 
+    height: 3, 
+    type: ['flying', 'poision']
+    }
+]
+    //Each object inside array is denoted with the placeholder value i. Using the dot operator specifies which property I want to display
+    for (let i = 0; i < pokemonList.length; i++) {
+        document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+        if (pokemonList[i].height > 3) {
+            document.write(' - Wow! That\'s a big one!');
+        }
+        }
+        
