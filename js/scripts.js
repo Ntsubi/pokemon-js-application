bb1fa1e516c11e9bdc9e100592dc9d26c432dba1
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-06 16:19:23 +0200

    Added comments to the code

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,12 @@ let pokemonList = [
     type: ['flying', 'poision']
     }
 ]
-    //Each object inside array is denoted with the placeholder value i. Using the dot operator specifies which property I want to display
+    //Index array 0 is our starting point. Value i will keep iterating by an increment of 1 until its run through all objects inside array
     for (let i = 0; i < pokemonList.length; i++) {
         document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+    //Code below instructs browser to test for the condition inside parantheses and execute the code block if condition met
         if (pokemonList[i].height > 3) {
-            document.write(' - Wow! That\'s a big one!');
+            document.write(' - Wow! That\'s a big one!'); // document.write prints to the DOM unlike console.log which is not visible inside DOM
         }
         }
         
