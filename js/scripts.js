83cd6131b7e21979282308b74fd531e9d6a0f5f3
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-11 18:32:17 +0200

    Replaced the for loop with a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,18 @@ let pokemonList = [
     type: ['flying', 'poision']
     }
 ]
-    //Index array 0 is our starting point. Value i will keep iterating by an increment of 1 until its run through all objects inside array
-    for (let i = 0; i < pokemonList.length; i++) {
-        document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
-    //Code below instructs browser to test for the condition inside parantheses and execute the code block if condition met
-        if (pokemonList[i].height > 3) {
-            document.write(' - Wow! That\'s a big one!'); // document.write prints to the DOM unlike console.log which is not visible inside DOM
-        }
-        }
+    pokemonList.forEach (function (pokemon) {
+        document.write(pokemon.name + " " + "(height: " + pokemon.height + ")")
+    })
+
+
+
+
         
+//Index array 0 is our starting point. Value i will keep iterating by an increment of 1 until its run through all objects inside array
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+//Code below instructs browser to test for the condition inside parantheses and execute the code block if condition met
+    // if (pokemonList[i].height > 3) {
+    //     document.write(' - Wow! That\'s a big one!'); // document.write prints to the DOM unlike console.log which is not visible inside DOM
+    
