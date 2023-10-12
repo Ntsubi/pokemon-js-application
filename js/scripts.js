4545d412e068bbc4d35103e56dfc2490de2472f1
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-12 18:27:35 +0200

    Fixed syntax error inside the parantheses

--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,7 @@ let pokemonRepository = (function () {
 
 console.log(pokemonRepository.getAll())
 pokemonRepository.getAll().forEach(function (pokemon) {
-    let pokemonList = document.querySelector('pokemon-list');
+    let pokemonList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = 'pokemon.name';
@@ -51,9 +51,3 @@ console.log(pokemonRepository.getAll())
 
 
 
-//Index array 0 is our starting point. Value i will keep iterating by an increment of 1 until its run through all objects inside array
-// for (let i = 0; i < pokemonList.length; i++) {
-//     document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
-//Code below instructs browser to test for the condition inside parantheses and execute the code block if condition met
-// if (pokemonList[i].height > 3) {
-//     document.write(' - Wow! That\'s a big one!'); // document.write prints to the DOM unlike console.log which is not visible inside DOM
