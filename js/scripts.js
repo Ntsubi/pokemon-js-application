7bfd20a3cd06af621d7d3aa5fe20cafb112288ea
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-15 17:36:31 +0200

    Added loadList function

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,23 @@ let pokemonRepository = (function () {
         button.classList.add('button-class');
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
-
         button.addEventListener('click', function () {
             alert(`You have selected ${pokemon.name}!`);
+        })
+    }
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
         })
     }
 
