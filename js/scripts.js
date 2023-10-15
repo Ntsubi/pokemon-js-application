a30330410bcaf90f5c9180d101731c89ba3a51bd
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-10-15 16:30:57 +0200

    Deleted content of pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,6 @@
 let pokemonRepository = (function () {
 
-    let pokemonList = [
-        {
-            name: 'Squirtle',
-            height: 5,
-            type: ['water', 'grass']
-        },
-        {
-            name: 'Diglet',
-            height: 2,
-            type: ['ground', 'trees']
-        },
-        {
-            name: 'Pidgey',
-            height: 3,
-            type: ['flying', 'poision']
-        }
-    ]
+    let pokemonList = []
 
     function getAll() {
         return pokemonList;
