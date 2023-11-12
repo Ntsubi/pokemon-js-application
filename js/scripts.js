3bcd3678efe61555ccd80f08b71cc46a5f4d701b
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-11-12 14:25:52 +0100

    Removed modal function code from scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -71,29 +71,6 @@ let pokemonRepository = (function () {
         });
     }
 
-    function showModal(pokemon) {
-
-        const modalBody = $('.modal-body');
-        const modalTitle = $('.modal-title');
-
-        //Calling the empty function clears exisiting content of the modal
-        modalTitle.empty();
-        modalBody.empty();
-
-        const nameElement = $('<h2>' + pokemon.name + '</h2>');
-        const imageElement = $('<img class = "modal-img" style =width:40%>');
-        imageElement.attr("src", pokemon.imageUrl);
-        const heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');
-        const weightElement = $('<p>' + 'Weight: ' + pokemon.weight + '</p>');
-        const typesElement = $('<p>' + 'Types: ' + pokemon.types + '</p>');
-
-        modalTitle.append(nameElement);
-        modalBody.append(imageElement);
-        modalBody.append(heightElement);
-        modalBody.append(weightElement);
-        modalBody.append(typesElement);
-    }
-
     //When called, the function pokemonRepository will return the object inside the curly braces. Because key and property are identical, syntax can be written in this format
     return {
         getAll,
